1f2f0def67c3c645f1b013dc624fcfe54b32608f
Vitor Macedo <vitorjvbm@gmail.com>
2022-05-20 17:36:26 -0300

    poll results

--- src/controllers/resultPollController.js
+++ src/controllers/resultPollController.js
@@ -1,14 +1,22 @@
 import db from "../../db.js";
+import { ObjectId } from "mongodb";
 
 export async function resultPoll(req, res){
-    const winChoice = {
-        title: "",
-        votes: 0
-    }
-
+    const pollId = req.params.id
     try{
-        const poll = await db.collection("polls").findOne();
-        if (!poll){return res.status(404).send("Enquete não encontrada")}
+        const poll = await db.collection("polls").findOne({_id: new ObjectId(pollId)})
+        if(!poll){return res.status(404).send("Enquete não encontrada")}
+
+        const choices = await db.collection("choices").find({pollId: pollId}). toArray();
+        if(!choices){return res.send("Erro ao buscar as alternativas")}
+
+        const result = {
+            poll: poll,
+            choices: choices
+        }
+
+        res.status(200).send(result)
+
     }catch(error){
         return res.send(error)
     }
