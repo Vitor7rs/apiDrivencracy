8d52dfbc06b4cfe3fe16f54fbf247e8df2a3b657
Vitor Macedo <vitorjvbm@gmail.com>
2022-05-20 15:13:06 -0300

    get poll choices

--- src/controllers/getChoicesController.js
+++ src/controllers/getChoicesController.js
@@ -1,11 +1,13 @@
 import db from "./../../db.js"
 
 export async function getChoices(req, res){
-
     try{
-        const choices = await db.collection("choices").find().toArray();
-        res.send(choices)
+        const choices = await db.collection("choices").find({pollId:req.params.id}).toArray();
+        if(choices.length===0){return res.status(404).send("Enquete não encontrada")}
+        return res.send(choices)
     }catch(error){
+        res.send(404)
         res.send(error)
+        return;
     }
 }
